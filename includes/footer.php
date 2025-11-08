dbcb4e389d1bff5225cebad11044da4892141622
trhieuuu29 <tr.hieucyrus.93@gmail.com>
2025-11-08 09:24:57 +0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,19 @@
-
+    <footer class="bg-dark text-white py-4 mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5><i class="fas fa-graduation-cap"></i> EduMatch</h5>
+                    <p>Kết nối sinh viên với cơ hội học bổng tốt nhất</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <p>&copy; 2024 EduMatch. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
